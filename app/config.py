16361b987924ba09ad65ddd32fcd145bd06a978b
Your Name Here <Same Email as used for github>
2026-01-07 23:06:35 +0200

    fix: start the schema file

--- app/config.py
+++ app/config.py
@@ -3,5 +3,13 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MODEL_PATH: str = os.getenv("MODEL_PATH")
+    RULES_LIST = ['']
+    system_prompt = '''You are given a comment from reddit and a rule. 
+        Your task is to classify whether the comment violates the rule. 
+        Only respond Yes/No.'''
+    positive = "Yes"
+    negative = "No"
+    judge_words = "Violation:"
+    subreddit = "politic"
 
 settings = Settings()
--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,35 @@
-from pydantic import BaseModel , Field
-
+from pydantic import BaseModel , Field   , field_validator
+from app.config import settings
 
 
 class ToxicityRequest(BaseModel):
-    ruleNum: int = Field(ge=0, le=6 , description=)
-    text: str = Field(min_length=2, max_length=512)
+    ruleNum: int = Field(ge=0)
 
-
+    @field_validator('ruleNum')
+    @classmethod
+    def check_rule_num_exist(cls, v: int) -> int:
+        if v >len(settings.RULES_LIST):
+            raise ValueError('rule num not found')
+        return v
+    
+    text: str = Field(min_length=1)
+    positive_example: str = Field(min_length=1)
+    negative_example: str = Field(min_length=1)
+    
 class ToxicityResponse(BaseModel):
     
     toxicity_score: float= Field(ge=0, le=1)
 
-def build_promt(request: ToxicityRequest)
+def build_prompt(request: ToxicityRequest):
+    rule = settings.RULES_LIST[request.ruleNum - 1]
+
+    return f"""{settings.system_prompt}
+        Subreddit: r/{settings.subreddit}
+        Rule: {rule}
+        Examples:
+        1) {request.positive_example}
+        {settings.judge_words} Yes
+        2) {request.negative_example}
+        {settings.judge_words} No
+        Comment: {request.text}
+        {settings.judge_words}"""
